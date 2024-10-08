b013893c934f353bcacfc5b6e64a2b4d158d2fe6
ThomasDlz <thomas.delzenne@gmail.com>
2024-10-08 11:24:42 +0200

    Calendar

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
 var myHeaders = new Headers();
 myHeaders.append("x-rapidapi-key", "8ae98bda88d2c921eca991683ada6ce7");
 myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
@@ -17,26 +21,23 @@ fetch(
   .catch((error) => console.log("error", error));
 
 function Home() {
+  const [value, setValue] = useState(new Date());
   return (
     <>
       <div
-        className="hero min-h-screen"
+        className="hero place-items-start min-h-[calc(100vh-4.3rem)]"
         style={{
           backgroundImage:
             "url(https://lelisia.com/cdn/shop/products/night-soccer-stadium-render.webp?v=1699408348)",
         }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
+        <div className="hero-overlay none bg-opacity-60"></div>
+        <div className="p-4 md:container md:mx-auto">
+          <Calendar
+            className="rounded-3xl bg-base-100/0  hidden lg:block lg:w-72 xl:w-80 2xl:w-1/4"
+            onChange={setValue}
+            value={value}
+          />
         </div>
       </div>
     </>
